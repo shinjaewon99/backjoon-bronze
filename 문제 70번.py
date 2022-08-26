f79a3a762bb1c9c58850ab1323b4d2de65c307fd
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-08-26 10:28:34 +0900

    Add files via upload

---
+++ 문제 70번.py
@@ -0,0 +1,14 @@
+# 14470번
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+if a < 0:
+    time = -a * c + d + b * e
+else:
+    time = (b - a) * e
+
+print(time)
